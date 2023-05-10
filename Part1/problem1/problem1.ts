3b55940fedb4223ac40a100a7c2d8bebeb7917c2
ALTA-FE14-Robbi <robbiromadhoni@gmail.com>
2023-05-11 00:11:01 +0700

    mengerjakan tugas problem 1 dan 2. problem 3 menyusul

--- Part1/problem1/problem1.ts
+++ Part1/problem1/problem1.ts
@@ -1,3 +1,18 @@
+// Program Acronym
+
 export const acronym = (phrase: string) => {
-  // your code here
-};
+  const words = phrase.split(' ');
+  let acronym = '';
+
+  for (let i = 0; i < words.length; i++) {
+    const firstLetter = words[i].charAt(0);
+    acronym += firstLetter.toUpperCase();
+  }
+
+  return acronym;
+}
+
+
+// console.log(acronym("Portable Network Graphics")); // PNG
+// console.log(acronym("Correct me if I'm wrong")); // CMIIW
+// console.log(acronym("Thanks god It's Friday")); // TGIF
--- Part1/problem2/problem2.ts
+++ Part1/problem2/problem2.ts
@@ -1,3 +1,20 @@
 export const decryptAtbash = (cipherText: string) => {
-  // your code here
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let plainText = "";
+  for (let i = 0; i < cipherText.length; i++) {
+    const char = cipherText[i];
+    const index = alphabet.indexOf(char.toLowerCase());
+    if (index === -1) {
+      // the character is not a letter, ignore it
+    } else {
+      const reverseIndex = alphabet.length - index - 1;
+      const reverseChar = alphabet[reverseIndex];
+      plainText += (char === char.toUpperCase()) ? reverseChar.toUpperCase() : reverseChar;
+    }
+  }
+  return plainText.replace(/\s+/g, "");
 };
+
+
+// console.log(decryptAtbash("Olivn rkhfn wloli hrg znvg"));
+// Loremipsumdolorsitamet
