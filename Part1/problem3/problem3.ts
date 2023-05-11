31a687ea3b418c722a0d4bf10e4a830c80b675ba
ALTA-FE14-Robbi <robbiromadhoni@gmail.com>
2023-05-11 09:04:38 +0700

    tugas problem 3

--- Part1/problem3/problem3.ts
+++ Part1/problem3/problem3.ts
@@ -1,3 +1,11 @@
 export const isArmstrongNumber = (num: number) => {
-  // your code here
-};
+  const digits = num.toString().split('');
+  const numDigits = digits.length;
+  const sum = digits.reduce((acc, digit) => {
+    return acc + Math.pow(parseInt(digit), numDigits);
+  }, 0);
+  return sum === num;
+}
+
+
+// console.log(isArmstrongNumber(153)); // true
